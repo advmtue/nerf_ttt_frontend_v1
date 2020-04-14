b98a8da4d9167aef83d44bce3fdc3e11f5314874
Adam Tuechler <adamtuechler@gmail.com>
2020-04-14 20:50:24 +1000

    Playerlist funcationlity

--- src/app/playerlist/playerlist.component.ts
+++ src/app/playerlist/playerlist.component.ts
@@ -9,14 +9,35 @@ import { ApiService } from '../api.service';
 export class PlayerlistComponent implements OnInit {
 
 	playerList: any;
+	filterList: any;
+	activeList: any;
+	searchName: string = '';
 
-	constructor(private apiService: ApiService) { }
+	constructor(private apiService: ApiService) {
+		this.playerList = [];
+		this.activeList = [];
+		this.filterList = [];
+	}
 
 	ngOnInit(): void {
 		this.apiService.playerList()
 		.subscribe((players: any) => {
 			this.playerList = players;
-			console.log(this.playerList);
+			this.activeList = this.playerList;
 		});
+	}
+
+	updateFilter() {
+		this.filterList = this.playerList.filter((pl) => {
+			let name = pl.first_name + pl.last_name;
+			name = name.toLowerCase();
+			return name.indexOf(this.searchName) > -1;
+		});
+
+		if (this.searchName === '') {
+			this.activeList = this.playerList;
+		} else {
+			this.activeList = this.filterList;
+		}
 	}
 }
