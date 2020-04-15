a73bde858113bf59a443eb7daccf254dbf047d10
Adam Tuechler <adamtuechler@gmail.com>
2020-04-15 17:10:16 +1000

    Exposed userService as public on navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -8,7 +8,7 @@ import { UserService } from '../user.service';
 })
 export class NavbarComponent implements OnInit {
 
-	constructor(private userService: UserService) {
+	constructor(public userService: UserService) {
 	}
 
 	ngOnInit(): void {
